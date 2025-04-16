d5360917c06b76630a2da806100ebde4ebc88c93
aidocons <consultant-butlyai@aidodev.com>
2025-04-16 20:08:09 +0100

    add cloudbuild plugin repo and version

--- cicd-plugins.go
+++ cicd-plugins.go
@@ -23,6 +23,8 @@ type CicdInterface interface {
 
 type PigenSteps struct {
 	Type string `yaml:"type" json:"type"`
+	Version string `yaml:"version" json:"version"`
+	RepoUrl string `yaml:"repo_url" json:"repo_url"`
 	Config map[string]any `yaml:"config" json:"config"`
 	Steps []Step `yaml:"steps" json:"steps"`
 }
