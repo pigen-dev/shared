2ca5d2fa99414dcafb3ce15e67468f027fded7e4
aidocons <consultant-butlyai@aidodev.com>
2025-04-22 14:35:37 +0100

    add notification struct

---
+++ cicd-notifier-plugins.go
@@ -0,0 +1,9 @@
+package pluginshared
+
+type PipelineNotification struct {
+	CicdType string `json:"cicd_type"`
+	RepoUrl string `json:"repo_url"`
+	Branch string `json:"branch"`
+	Status string `json:"status"`
+	Metadata map[string]string `json:"metadata"`
+}
